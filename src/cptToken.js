8eaf1bbe50df83b46546c93ffcc92862566f9bf4
NaumM09 <mahlatse.modiba@gmail.com>
2025-02-07 08:52:34 +0200

    reduced size

--- src/cptToken.js
+++ src/cptToken.js
@@ -15,7 +15,6 @@ const CPTToken = () => {
         "/assets/video1.mp4",
         "/assets/video2.mp4",
         "/assets/video3.mp4",
-        "/assets/video4.mp4",
         "/assets/video5.mp4",
         "/assets/video6.mp4",
     ];
